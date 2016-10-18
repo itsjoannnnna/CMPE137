0156006ef9bd41275283c6117014be137102d99c
itsjoannnnna <reyes.joannamarie@gmail.com>
2016-10-17 19:28:47 -0700

    Adding bullets

--- shootingGame/GameScene.swift
+++ shootingGame/GameScene.swift
@@ -18,11 +18,17 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         Player.position = CGPoint(x: self.size.width/12, y: self.frame.size.height/1000)
-        
+        var time = Timer.scheduledTimerWithTimeInterval(0.2, target: self, selector: Selector(shootBullets()), userInfo: nil, repeats: true)
         self.addChild(Player)
 
+    }
+    
+    func shootBullets(){
+        var Bullet = SKSpriteNode(imageNamed: "bullet.png")
+        Bullet.zPosition = -5
+        Bullet.position = CGPoint(x: Player.position.x, y: Player.position.y)
+        self.addChild(Bullet)
         
-
     }
     
     
