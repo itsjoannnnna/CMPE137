c8f70ad6b3d21904b17e3c397a5d136365f87765
itsjoannnnna <reyes.joannamarie@gmail.com>
2016-10-18 14:29:27 -0700

    Adding score and started the ending game section

--- shootingGame/GameScene.swift
+++ shootingGame/GameScene.swift
@@ -9,26 +9,121 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+struct PhysicsCategory{
+    static let Aliens: UInt32 = 1 //lets the last bit equal to 1
+    static let Bullet: UInt32 = 2 // lets the last bits equal to 2
+    static let Player: UInt32 = 3 //lets the last bits equal to 3
+}
+
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    var Score = Int()
     
     @IBInspectable
     var Player = SKSpriteNode(imageNamed: "rocket.png")
+    
+    var ScoreLabel = UILabel()
+    
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
-        Player.position = CGPoint(x: self.size.width/12, y: self.frame.size.height/1000)
-        var time = Timer.scheduledTimerWithTimeInterval(0.2, target: self, selector: Selector(shootBullets()), userInfo: nil, repeats: true)
+        
+        physicsWorld.contactDelegate = self
+        
+        Player.position = CGPoint(x: self.size.width/12, y: -self.frame.size.height/3)
+        Player.physicsBody = SKPhysicsBody(rectangleOf: Player.size)
+        Player.physicsBody?.affectedByGravity = false
+        
+        Player.physicsBody?.categoryBitMask = PhysicsCategory.Player
+        Player.physicsBody?.contactTestBitMask = PhysicsCategory.Aliens
+        Player.physicsBody?.isDynamic = false
+        
+        _ = Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(self.shootBullets), userInfo: nil, repeats: true) //increase the amount of time the bullets will come out of the rocket ship.
+            //this is the timer for the bullets
+        
+        _ = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(self.shootAlienEnemies), userInfo: nil, repeats: true) //increase more enemies at any certain time, decrease time intervale < 1.0
+            //this is the timer for the enemies that are falling
         self.addChild(Player)
+        
+        
+        //adds the score label to the top of the screen
+        ScoreLabel.text = "\(Score)"
+        ScoreLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 20))
+        ScoreLabel.backgroundColor = UIColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 0.3)
+        self.view?.addSubview(ScoreLabel)
+
+    }
+
+    func didBegin(_ contact: SKPhysicsContact) {
+        let firstBody: SKPhysicsBody = contact.bodyA //takes first body that hits and do stuff with it
+        let secondBody: SKPhysicsBody = contact.bodyB //takes second body that hits
+        
+        //checks if the bullets have hit the aliens
+        if((firstBody.categoryBitMask == PhysicsCategory.Aliens && secondBody.categoryBitMask == PhysicsCategory.Bullet) || (firstBody.categoryBitMask == PhysicsCategory.Bullet && secondBody.categoryBitMask == PhysicsCategory.Aliens)){
+            collisionWithBullet(Alien: firstBody.node as! SKSpriteNode, Bullet: secondBody.node as! SKSpriteNode)
+        }
+            
+        //checks if the alien hit the players
+        else if((firstBody.categoryBitMask == PhysicsCategory.Aliens && secondBody.categoryBitMask == PhysicsCategory.Player) || (firstBody.categoryBitMask == PhysicsCategory.Player && secondBody.categoryBitMask == PhysicsCategory.Aliens)){
+            collisionWithPlayer(Alien: firstBody.node as! SKSpriteNode, Player: secondBody.node as! SKSpriteNode)
+        }
 
     }
     
+    func collisionWithBullet(Alien: SKSpriteNode, Bullet: SKSpriteNode){
+        Alien.removeFromParent()
+        Bullet.removeFromParent()
+        Score += 1
+        ScoreLabel.text = "Score: \(Score)"
+    }
+    
+    func collisionWithPlayer(Alien: SKSpriteNode, Player: SKSpriteNode){
+        Alien.removeFromParent()
+        Player.removeFromParent()
+    }
+    
+    
     func shootBullets(){
-        var Bullet = SKSpriteNode(imageNamed: "bullet.png")
+        let Bullet = SKSpriteNode(imageNamed: "bullet.png")
         Bullet.zPosition = -5
         Bullet.position = CGPoint(x: Player.position.x, y: Player.position.y)
+        
+        let shooting = SKAction.moveTo(y: self.size.height + 30, duration: 1.0)
+        let shootingDone = SKAction.removeFromParent()
+        Bullet.run(SKAction.sequence([shooting, shootingDone]))
+        //Bullet.run(SKAction.repeatForever(shooting))
+        
+        Bullet.physicsBody = SKPhysicsBody(rectangleOf: Bullet.size)
+        Bullet.physicsBody?.categoryBitMask = PhysicsCategory.Bullet
+        Bullet.physicsBody?.contactTestBitMask = PhysicsCategory.Aliens //bullets go away from the
+        Bullet.physicsBody?.affectedByGravity = false // so the bullets don't fly off
+        Bullet.physicsBody?.isDynamic = false
         self.addChild(Bullet)
         
+    }
+    
+    func shootAlienEnemies(){
+        let Aliens = SKSpriteNode(imageNamed: "alien.png")
+        let minValue = self.size.width/100
+        let maxValue = self.size.width - 30 // makes sure it doesn't go off scene
+        let spawnPoint = UInt32(maxValue - minValue)
+        Aliens.position = CGPoint(x: CGFloat(arc4random_uniform(spawnPoint)), y: self.size.height)
+        
+        let fallFromSky = SKAction.moveTo(y: -self.frame.size.height, duration: 5.0) //for the levels, we can decrease the duration to make it faster
+        Aliens.run(SKAction.repeatForever(fallFromSky))
+        
+        Aliens.physicsBody = SKPhysicsBody(rectangleOf: Aliens.size)
+        Aliens.physicsBody?.categoryBitMask = PhysicsCategory.Aliens
+        Aliens.physicsBody?.contactTestBitMask = PhysicsCategory.Bullet
+        Aliens.physicsBody?.affectedByGravity = false
+        Aliens.physicsBody?.isDynamic = true
+        
+        let fallFromSkyDone = SKAction.removeFromParent()
+        Aliens.run(SKAction.sequence([fallFromSky, fallFromSkyDone]))
+        
+        self.addChild(Aliens)
     }
     
     
