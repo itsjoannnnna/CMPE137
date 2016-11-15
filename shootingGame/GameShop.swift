f4a5995aac71fce519b0c4cebe3a0e6bf2c280d6
Elaine Yu <laineeyu@gmail.com>
2016-11-15 14:09:45 -0800

    Testing from own laptop (windows). Changed GameScene to GameScene1 because it always starts from GameScene file. So GameScene file is the homescreen with buttons for start game, infinite game mode, and shop. Currently all the button is using temporary StartButton Image. Gameshop screen is up with a title and return button to homescreen. After a game, should be able to restart or return to homescreen.

---
+++ shootingGame/GameShop.swift
@@ -0,0 +1,43 @@
+import Foundation
+import SpriteKit
+
+
+class GameShop: SKScene {
+    
+    var TestingLabel : UILabel!
+    var returnBtn : UIButton!
+    
+    override func didMove(to view: SKView) {
+        //Backgrount color
+        scene?.backgroundColor = UIColor.white
+        
+        //Label for Shop
+        TestingLabel = UILabel(frame:CGRect(x: 0, y:0, width: view.frame.size.width/2, height: 30))
+        TestingLabel.textColor = UIColor.red
+        TestingLabel.font = UIFont.boldSystemFont(ofSize: 32.0)
+        TestingLabel.center = CGPoint(x: view.frame.size.width/2, y: view.frame.size.width/5)
+        TestingLabel.text = "Shop!"
+        self.view?.addSubview(TestingLabel)
+        
+        
+        //Return button for going back to HomeScreen
+        returnBtn = UIButton (frame: CGRect(x: 0, y:0, width: view.frame.size.width/3, height: 30))
+        returnBtn.center = CGPoint(x: view.frame.midX-160, y: view.frame.midY-350)
+        
+        returnBtn.setTitle("Return", for: UIControlState.normal)
+        returnBtn.setTitleColor(UIColor.darkGray, for: UIControlState.normal)
+        returnBtn.addTarget(self, action: #selector(GameShop.Return), for: UIControlEvents.touchUpInside)
+        self.view!.addSubview(returnBtn)
+    }
+
+    //Return function to redirect scene to HomeScreen
+    func Return(){
+        //move to specified scene
+        let scene = GameScene(fileNamed: "GameScene")
+        //starting transition between scenes
+        self.view?.presentScene(scene)
+        TestingLabel.removeFromSuperview()
+        returnBtn.removeFromSuperview()
+
+    }
+}
